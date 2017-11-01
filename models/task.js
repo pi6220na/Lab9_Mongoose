c1f1fdee30bb752ea7f7e958a2ace54754efc923
pi6220na <myrlin2002@hotmail.com>
2017-11-01 14:36:33 -0500

    Convert to Mongoose

---
+++ models/task.js
@@ -0,0 +1,17 @@
+/**
+ * Created by pi6220na on 11/1/2017.
+ */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//define your schema
+var taskSchema = new Schema( {
+    text: String,
+    completed: Boolean
+});
+
+// compile taskSchema into Mongoose model object
+var Task = mongoose.model('Task', taskSchema);
+
+// export the Task so our other code can use it
+module.exports = Task;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var ObjectID = require('mongodb').ObjectId;
 var router = express.Router();
+var Task = require('../models/task');
+
 
 ///* GET home page. */
 //router.get('/', function(req, res, next) {
@@ -9,6 +11,7 @@ var router = express.Router();
 
 
 /* GET home page with all incomplete tasks */
+/*
 router.get('/', function(req, res, next) {
 
     req.tasks.find( {completed:false} ).toArray().then( (docs) => {
@@ -18,6 +21,21 @@ router.get('/', function(req, res, next) {
     });
 
 });
+*/
+
+router.get('/', function(req, res, next) {
+
+  Task.find( {completed: false})
+      .then( (docs) => {
+          res.render('index', {title: 'Incomplete Tasks', tasks: docs})
+      }).catch( (err) => {
+          next(err);
+  });
+
+});
+
+
+
 
 /* POST new task */
 router.post('/add', function(req, res, next){
@@ -30,12 +48,22 @@ router.post('/add', function(req, res, next){
 
     else {
         // Insert into database. New tasks are assumed to be not completed.
+        /*
         req.tasks.insertOne({text: req.body.text, completed: false})
             .then(() => {
                 res.redirect('/');
             })
             .catch((err) => {
                 next(err);   // most likely to be a database error.
+            });
+            */
+        new Task( { text: req.body.text, completed: false} ).save()
+            .then((newTask) => {
+            console.log('The new task created is: ', newTask);
+            res.redirect('/');
+            })
+            .catch((err) => {
+                next(err);  // database error?
             });
     }
 
@@ -45,6 +73,20 @@ router.post('/add', function(req, res, next){
 /* POST task done */
 router.post('/done', function(req, res, next){
 
+    Task.findOneAndUpdate( {_id: req.body._id}, {$set: {completed: true}} )
+        .then((updatedTask) => {
+            if (updatedTask) {   // updatedTask is the document *before* the update
+                res.redirect('/')  // One thing was updated. Redirect to home
+            } else {
+                // if no updatedTask, then no matching document was found to update. 404
+                res.status(404).send("Error marking task done: not found");
+            }
+        }).catch((err) => {
+        next(err);
+    })
+
+
+    /*
     var _id = req.body._id;
 
     if (!ObjectID.isValid(_id)) {
@@ -71,12 +113,15 @@ router.post('/done', function(req, res, next){
             });
     }
 
+    */
+
 });
 
 
 /* POST all tasks done */
 router.post('/alldone', function(req, res, next) {
 
+    /*
     req.tasks.updateMany( { completed : false } , { $set : { completed : true}} )
         .then( (result) => {
             res.redirect('/');
@@ -84,6 +129,17 @@ router.post('/alldone', function(req, res, next) {
         .catch( (err) => {
             next(err);
         })
+     */
+
+     Task.updateMany( { completed : false } , { $set : { completed : true} } )
+     .then( (result) => {
+     console.log("How many documents were modified? ", result.n);
+     req.flash('info', 'All tasks marked as done!');
+     res.redirect('/');
+     })
+     .catch( (err) => {
+     next(err);
+     })
 
 });
 
@@ -93,17 +149,28 @@ router.post('/alldone', function(req, res, next) {
 /* GET completed tasks */
 router.get('/completed', function(req, res, next){
 
+    /*
     req.tasks.find( {completed:true} ).toArray()
         .then( (docs) => {
             res.render('tasks_completed', {title: 'Completed Tasks', tasks: docs });
         }).catch( (err) => {
         next(err);
     });
+    */
+
+    Task.find( {completed:true} )
+        .then( (docs) => {
+            res.render('tasks_completed', { title: 'Completed tasks', tasks: docs });
+        }).catch( (err) => {
+            next(err);
+    });
+
 });
 
 /* POST task delete */
 router.post('/delete', function(req, res, next){
 
+    /*
     var _id = req.body._id;
 
     if (!ObjectID.isValid(_id)) {
@@ -128,6 +195,24 @@ router.post('/delete', function(req, res, next){
                 next(err);
             });
     }
+    */
+
+
+    Task.deleteOne( { _id : req.body._id } )
+        .then( (result) => {
+
+            if (result.deletedCount === 1) {  // one task document deleted
+                res.redirect('/');
+
+            } else {
+                // The task was not found. Report 404 error.
+                res.status(404).send('Error deleting task: not found');
+            }
+        })
+        .catch((err) => {
+
+            next(err);   // Will handle invalid ObjectIDs or DB errors.
+        });
 
 });
 
@@ -135,6 +220,7 @@ router.post('/delete', function(req, res, next){
 /* GET details about one task */
 router.get('/task/:_id', function(req, res, next) {
 
+    /*
     var _id = req.params._id;
 
     if (!ObjectID.isValid(_id)) {
@@ -158,6 +244,31 @@ router.get('/task/:_id', function(req, res, next) {
                 next(err);
             })
     }
+    */
+
+
+    /* This route matches URLs in the format task/anything
+     Note the format of the route path is  /task/:_id
+     This matches task/1 and task/2 and task/3...
+     Whatever is after /task/ will be available to the route as req.params._id
+     For our app, we expect the URLs to be something like task/1234567890abcdedf1234567890
+     Where the number is the ObjectId of a task.
+     So the req.params._id will be the ObjectId of the task to find
+     */
+
+    Task.findOne({_id: req.params._id} )
+        .then( (task) => {
+            if (task) {
+                res.render('task', {title: 'Task', task: task});
+            } else {
+                res.status(404).send('Task not found');
+            }
+        })
+        .catch((err) => {
+            next(err);
+        })
+
+
 });
 
 
